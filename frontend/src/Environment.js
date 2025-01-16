42f7ca3232be5c6ff4d5066a47793d41574c2214
Marcos Paulo <42106860+TekhnoSoft@users.noreply.github.com>
2025-01-16 18:21:43 -0300

    Update Environment.js

--- frontend/src/Environment.js
+++ frontend/src/Environment.js
@@ -1,7 +1,7 @@
 const DEVELOPMENT_MODE = false;
 
 const Environment = {
-    API_BASE: (DEVELOPMENT_MODE) ? "http://localhost:3001" : "https://tekhno-projects-ticket-digital-backend.7richv.easypanel.host/",
+    API_BASE: (DEVELOPMENT_MODE) ? "http://localhost:3001" : "https://tekhno-projects-ticket-digital-backend.7richv.easypanel.host",
     HEADERS: { 
         headers: { 
             TICKETDIGITAL_ACCESS_TOKEN : localStorage.getItem("TICKETDIGITAL_ACCESS_TOKEN")
